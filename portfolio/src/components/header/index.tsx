a2cd5300107b623180f140b74d9d94733a3aff12
Arron <UP2044509@myport.ac.uk>
2022-11-05 23:57:15 +0000

    centering header text

--- portfolio/src/components/header/index.tsx
+++ portfolio/src/components/header/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 const Header = () => (
 	<div className="flex h-20 shadow-md w-full justify-end items-center border-b-[1px] border-b-slate-600">
 		<Link
-			className="font-bold text-3xl mr-auto ml-4 lg:text-5xl lg:mx-auto cursor-pointer"
+			className="flex font-bold mr-auto text-3xl ml-4 lg:text-5xl lg:mx-auto cursor-pointer lg:ml-14 lg:w-full lg:justify-center"
 			href="/"
 		>
 			Arron Lincoln
